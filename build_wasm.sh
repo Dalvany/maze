3d1e3d0d25bdb850070530edae0b931a5deaa94d
Dalvany <9901407+Dalvany@users.noreply.github.com>
2023-08-23 21:33:58 +0200

    Remove site directory

--- build_wasm.sh
+++ build_wasm.sh
@@ -3,3 +3,4 @@
 cargo build --profile release-wasm --target wasm32-unknown-unknown --features js
 wasm-bindgen --out-dir ./target/web --target web ./target/wasm32-unknown-unknown/release-wasm/maze-3d.wasm
 cp target/web/* site/
+cp assets/* site/
