add0915a544a7bd5f09fa20a4b05ad27e43e4252
Dalvany <9901407+Dalvany@users.noreply.github.com>
2023-08-23 21:34:29 +0200

    Fix wasm cp

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,5 +2,6 @@
 
 cargo build --profile release-wasm --target wasm32-unknown-unknown --features js
 wasm-bindgen --out-dir ./target/web --target web ./target/wasm32-unknown-unknown/release-wasm/maze-3d.wasm
+mkdir -p site/
 cp target/web/* site/
 cp assets/* site/
